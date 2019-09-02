9befe7e1ce82b1721e4e6c73c0633b87b8253b3b
Raj Dahal <rajdahal2010@gmail.com>
2019-09-02 09:10:35 -0400

    Initial commit

---
+++ src/main/java/com/ewh/smart/SmartkidApplication.java
@@ -0,0 +1,13 @@
+package com.ewh.smart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartkidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartkidApplication.class, args);
+	}
+
+}
