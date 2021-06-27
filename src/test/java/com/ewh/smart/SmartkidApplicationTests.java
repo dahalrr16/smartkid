d669c30ccf8027727a9dd07e03b80d637f79a844
DESKTOP-B0BAS2U\Sangita <syaang4r@gmail.com>
2021-06-27 11:41:06 -0400

    created a test failure

--- src/test/java/com/ewh/smart/SmartkidApplicationTests.java
+++ src/test/java/com/ewh/smart/SmartkidApplicationTests.java
@@ -2,6 +2,9 @@ package com.ewh.smart;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
+
+import com.ewh.smart.model.Kid;
 
 @SpringBootTest
 public class SmartkidApplicationTests {
@@ -9,8 +12,11 @@ public class SmartkidApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+	@SuppressWarnings("deprecation")
 	@Test
 	public void helloKid() {
 		System.out.println("Hello Kid just for fun");
+		Kid kid =new Kid();
+		Assert.isNull(kid);
 	}
 }
